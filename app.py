8997fbc3ef8b20e35a655489b3436ca9a5ee381f
Dylan Moore <moore.dylan.e@gmail.com>
2015-08-23 11:00:33 -0700

    Add website

--- app.py
+++ app.py
@@ -5,4 +5,8 @@ import bottle
 def index():
     return bottle.static_file('index.html', root="")
 
+@bottle.get('/static/<filepath:path>')
+def server_static(filepath):
+    return bottle.static_file(filepath, root='static')
+
 application = bottle.default_app()
