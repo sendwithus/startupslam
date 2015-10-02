7387eb10de031e88250f0f05b21feaf9d3d8215e
Brandon Brown <brandon@brandonb.io>
2015-10-02 15:21:21 -0700

    Add wufo form to feedback url

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ import bottle
 def index():
     return bottle.static_file('index.html', root="")
 
+@bottle.get('/feedback')
+def index():
+    return bottle.redirect('https://sendwithus.wufoo.com/forms/z1fxg2hi06l4k7h/')
+
 @bottle.get('/static/<filepath:path>')
 def server_static(filepath):
     return bottle.static_file(filepath, root='static')
