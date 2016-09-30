6329116cb951859c95be49a9543ed4ebe80387b5
Clarke Brunsdon <clarke@stembolt.com>
2016-09-29 19:21:12 -0700

    Add a calculator class to be filled out

---
+++ workshops-2016/junior/write_tests/calculator.py
@@ -0,0 +1,48 @@
+class Calculator:
+    def add(self, num1, num2):
+        """
+        Add together the two arguements and return the result
+        """
+        pass
+
+    def subtract(self, num1, num2):
+        """
+        Subtract the second arguement from the first
+        argument and return the result
+        """
+        pass
+
+    def reverse(self, number):
+        """
+        Reverse a number and return the result.
+
+        Decimal places should be respected, unless there are
+        no values after the decimal.
+        e.g. 12345 would return 54321
+             123.45 would return 54.321
+             98765.000 would return 56789
+        """
+        pass
+
+    def power(self, num1, num2):
+        """
+        Returns num1 to the power of num2
+        """
+        pass
+
+    def nobody_likes_hundreds(self, number):
+        """
+        Strips any values in the 100 column, replacing it with a zero
+
+        e.g. 13999 would return 13099
+        """
+        pass
+
+    def binary_string_to_decimal(self, string):
+        """
+        Takes a string containing a binary number
+        and returns it as an integer
+
+        e.g. '111' would return 7
+        """
+        pass
